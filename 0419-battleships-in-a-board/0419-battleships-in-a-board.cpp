a94429f1ffcb0d150555cbb9cca57381813b710e
Akash Raghav <89372258+raghav-akash@users.noreply.github.com>
2022-10-30 15:18:05 +0530

    Time: 8 ms (93.21%), Space: 8.1 MB (84.23%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-    void modify(vector<vector<char>>& board,int i,int j)
-    {
-        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j]!='X') 
-        {
-            return;
-        }
-        board[i][j]='.';
-        modify(board,i+1,j);
-        modify(board,i-1,j);
-        modify(board,i,j+1);
-        modify(board,i,j+1);
-        
-    }
-    
     int countBattleships(vector<vector<char>>& board) {
         int battleship=0;
         for(int i=0;i<board.size();i++)
         {
             for(int j=0;j<board[0].size();j++)
             {
-                if(board[i][j]=='X')
-                {
-                    battleship++;
-                    modify(board,i,j);
+                if(board[i][j]=='.')
+                { 
+                    continue;
                 }
+                if(i>0 && board[i-1][j]=='X')
+                {
+                    continue;
+                }
+                if(j>0 && board[i][j-1]=='X')
+                {
+                    continue;
+                }
+                battleship++;
             }
         }
         return battleship;
